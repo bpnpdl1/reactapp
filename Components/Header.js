5dd70a4a86661dc87394199d33f37de2af962b42
Bipin Paudel <bipo_bca2076@lict.edu.np>
2023-10-15 20:41:16 +0545

    first commit

---
+++ Components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <>
+         <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+            <div className="flex items-center flex-shrink-0 text-white mr-6">
+                <span className="font-semibold text-xl tracking-tight">React  Header Component</span>
+            </div>
+            </nav>
+        </>
+    );
+}
+
+export default Header;
